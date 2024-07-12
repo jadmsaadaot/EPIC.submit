1183f7997ce8948e96d65cf4fa05ac1ccca0704a
David Nunez <david.nunez@aot-technologies.com>
2024-07-12 12:59:16 -0400

    refactor root

--- submit-web/src/routes/__root.tsx
+++ submit-web/src/routes/__root.tsx
@@ -1,20 +1,6 @@
-import EAOAppBar from "@/components/EAOAppBar";
-import SideNavBar from "@/components/SideNavBar";
-import { Box } from "@mui/material";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import App from "@/App";
+import { createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <EAOAppBar />
-      <Box display={"flex"}>
-        <SideNavBar />
-        <Box display={"flex"} flexDirection={"column"}>
-          <Outlet />
-        </Box>
-      </Box>
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: () => <App />,
 });
