615845293d27c0092412e34250ea17a4816c3689
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-07-22 16:28:24 -0600

    fixing cypress test for CI

--- submit-web/src/App.cy.tsx
+++ submit-web/src/App.cy.tsx
@@ -42,7 +42,6 @@ describe("<App />", () => {
       // add other necessary mocks here
     };
     mount(<TestApp authentication={mockAuth} />);
-    cy.contains(AppConfig.appTitle);
     cy.contains("Users");
   });
 });
