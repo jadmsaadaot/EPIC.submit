672ef97101c8c331b9db6606720ef62112e79bdc
David Nunez <david.nunez@aot-technologies.com>
2024-08-15 12:28:25 -0400

    remove unused variables

--- submit-web/src/components/Shared/Drawers/DrawerStore.ts
+++ submit-web/src/components/Shared/Drawers/DrawerStore.ts
@@ -1,40 +1,27 @@
 import { create } from "zustand";
-import { User } from "@/models/User";
-import { Plan } from "@/models/Plan";
 import React from "react";
-
-// Define the DrawerData type
-export type DrawerData = {
-  user?: User;
-  plan?: Plan;
-};
 
 // Define the store state and actions
 interface DrawerStore {
-  data: DrawerData;
   isOpen: boolean;
   drawerContent: React.ReactNode | null;
   direction: "left" | "right" | "top" | "bottom";
   setOpen: (
     drawer: React.ReactNode,
     direction: "left" | "right" | "top" | "bottom",
-    fetchData?: () => Promise<DrawerData>,
   ) => Promise<void>;
   setClose: () => void;
 }
 
 // Create the Zustand store
 export const useDrawer = create<DrawerStore>((set) => ({
-  data: {},
   isOpen: false,
   drawerContent: null,
   direction: "left",
 
-  setOpen: async (drawer, direction, fetchData) => {
+  setOpen: async (drawer, direction) => {
     if (drawer) {
-      const fetchedData = fetchData ? await fetchData() : {};
-      set((state) => ({
-        data: { ...state.data, ...fetchedData },
+      set(() => ({
         drawerContent: drawer,
         isOpen: true,
         direction: direction,
@@ -45,7 +32,6 @@ export const useDrawer = create<DrawerStore>((set) => ({
   setClose: () => {
     set({
       isOpen: false,
-      data: {},
       drawerContent: null,
       direction: "left",
     });
@@ -56,8 +42,7 @@ export const useDrawer = create<DrawerStore>((set) => ({
 export const openDrawer = async (
   drawer: React.ReactNode,
   direction: "left" | "right" | "top" | "bottom",
-  fetchData?: () => Promise<DrawerData>,
 ) => {
   const { setOpen } = useDrawer.getState();
-  await setOpen(drawer, direction, fetchData);
+  await setOpen(drawer, direction);
 };
