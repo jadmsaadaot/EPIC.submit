aa55d589959a321e194cec14d71f98cea9558fcd
Jad <jad.saad@aot-technologies.com>
2024-08-19 14:01:29 -0700

    Add missing dependency to useEffect

--- submit-web/src/routes/registration.tsx
+++ submit-web/src/routes/registration.tsx
@@ -23,7 +23,7 @@ function Registration() {
         redirect_uri: `${OidcConfig.redirect_uri}?proponent_id=${proponent_id}`,
       });
     }
-  }, [proponent_id]);
+  }, [proponent_id, isAuthenticated]);
 
   if (isAuthenticated || !proponent_id) {
     return <Navigate to={"/error"} />;
