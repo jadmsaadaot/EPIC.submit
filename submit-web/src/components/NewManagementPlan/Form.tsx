4f3c010c1cfb3172b0fe289231c19a4a517e2bf6
Jad <jad.saad@aot-technologies.com>
2024-08-28 14:59:09 -0700

    add stepper buttons

--- submit-web/src/components/NewManagementPlan/Form.tsx
+++ submit-web/src/components/NewManagementPlan/Form.tsx
@@ -1,10 +1,34 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import TabStepper from "./TabStepper";
 import { TabPanel } from "./TabPanel";
+import { Unless, When } from "react-if";
+import { useManagementPlanForm } from "./formStore";
+import { stepLabels } from "./constants";
 
-export const Form = () => {
+type FormProps = {
+  handleSubmit: () => void;
+};
+export const Form = ({ handleSubmit }: FormProps) => {
+  const { step, setStep, reset } = useManagementPlanForm();
+
+  const handleNext = () => {
+    setStep(Math.min(step + 1, stepLabels.length - 1));
+  };
+  const handleBack = () => {
+    setStep(Math.max(step - 1, 0));
+  };
+
+  const handleCancel = () => {
+    reset();
+  };
+
+  const handleComplete = () => {
+    handleSubmit();
+    reset();
+  };
+
   return (
-    <Grid container width={"100%"}>
+    <Grid container width={"100%"} minHeight={"30em"}>
       <Grid
         item
         xl={8}
@@ -21,6 +45,39 @@ export const Form = () => {
         <Grid item xs={12}>
           <TabPanel />
         </Grid>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        container
+        direction={"row"}
+        alignItems={"center"}
+        spacing={2}
+        mt={"3em"}
+      >
+        <Grid item>
+          <Button variant="text" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </Grid>
+        <Unless condition={step === 0}>
+          <Grid item>
+            <Button color="secondary" onClick={handleBack}>
+              Back
+            </Button>
+          </Grid>
+        </Unless>
+        <Unless condition={step === stepLabels.length - 1}>
+          <Grid item>
+            <Button onClick={handleNext}>Next</Button>
+          </Grid>
+        </Unless>
+        <When condition={step === stepLabels.length - 1}>
+          <Grid item>
+            <Button onClick={handleComplete}>Complete Form</Button>
+          </Grid>
+        </When>
       </Grid>
     </Grid>
   );
