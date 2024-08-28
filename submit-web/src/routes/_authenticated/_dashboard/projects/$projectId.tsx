4255a41cde7b311d9004c5e18003d42be55a0e79
David Nunez <david.nunez@aot-technologies.com>
2024-08-28 16:18:30 -0400

    remove unused import

--- submit-web/src/routes/_authenticated/_dashboard/projects/$projectId.tsx
+++ submit-web/src/routes/_authenticated/_dashboard/projects/$projectId.tsx
@@ -3,11 +3,11 @@ import { useGetProject } from "@/hooks/api/useProjects";
 import { AccountProject } from "@/models/Project";
 import { useParams } from "@tanstack/react-router";
 import { Project as ProjectComponent } from "@/components/Projects/Project";
-import { Container, Stack } from "@mui/material";
+import { Container } from "@mui/material";
 import { ProjectsSkeleton } from "@/components/Projects";
 
 export const Route = createFileRoute(
-  "/_authenticated/_dashboard/projects/$projectId"
+  "/_authenticated/_dashboard/projects/$projectId",
 )({
   component: ProjectPage,
   meta: () => [{ title: "Project" }],
