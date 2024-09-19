8e317f6ebfe470f87e16d0e2e37150f172ca37f2
David Nunez <david.nunez@aot-technologies.com>
2024-09-19 12:39:19 -0400

    add get to the state

--- submit-web/src/store/documentUploadStore.ts
+++ submit-web/src/store/documentUploadStore.ts
@@ -15,23 +15,26 @@ const initialState = {
   documents: [],
 };
 
-export const useDocumentUploadStore = create<DocumentUploadState>((set) => ({
-  documents: [],
-  handleAddDocuments: (files: File[]) => {
-    // Add file processing logic here
-    if (files.length > 0) {
-      const file = files[0];
+export const useDocumentUploadStore = create<DocumentUploadState>(
+  (set, get) => ({
+    documents: [],
+    handleAddDocuments: (files: File[]) => {
+      // Add file processing logic here
+      if (files.length > 0) {
+        const file = files[0];
+        const currentDocuments = get().documents;
 
-      set({
-        documents: [
-          ...initialState.documents,
-          {
-            file,
-            isComplete: false,
-          },
-        ],
-      });
-    }
-  },
-  reset: () => set(initialState),
-}));
+        set({
+          documents: [
+            ...currentDocuments,
+            {
+              file,
+              isComplete: false,
+            },
+          ],
+        });
+      }
+    },
+    reset: () => set(initialState),
+  }),
+);
