c0f836d1e39c7c7326342c685e5e0a101aaf8af6
David Nunez <david.nunez@aot-technologies.com>
2024-10-21 10:46:09 -0400

    remove hard coded values

--- submit-web/src/routes/_authenticated/_dashboard/projects/index.tsx
+++ submit-web/src/routes/_authenticated/_dashboard/projects/index.tsx
@@ -24,7 +24,7 @@ export function ProjectsPage() {
     isPending: isProjectsLoading,
     isError: isProjectsError,
   } = useGetProjects({
-    accountId: 1,
+    accountId,
     searchOptions: filters,
   });
 
