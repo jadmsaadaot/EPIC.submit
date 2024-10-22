349202f65f780a287ec3822c144923a4b2ac7da3
David Nunez <david-nunez@aot-technologies.com>
2024-10-22 13:17:50 -0400

    refactor join

--- submit-api/src/submit_api/models/queries/project.py
+++ submit-api/src/submit_api/models/queries/project.py
@@ -15,6 +15,7 @@
 from submit_api.models import AccountProject, Project, db
 from submit_api.models.account_project_search_options import AccountProjectSearchOptions
 from submit_api.models.package import Package
+
 # pylint: disable=too-few-public-methods
 
 
@@ -22,11 +23,16 @@ class ProjectQueries:
     """Query module for complex projects queries"""
 
     @classmethod
-    def get_projects_by_account_id(cls, account_id: int, search_options=AccountProjectSearchOptions):
+    def get_projects_by_account_id(
+        cls, account_id: int, search_options=AccountProjectSearchOptions
+    ):
         """Find projects by account_id with optional search and pagination."""
-        query = db.session.query(AccountProject).filter(
-            AccountProject.account_id == account_id
-        ).join(Project)
+        query = (
+            db.session.query(AccountProject)
+            .filter(AccountProject.account_id == account_id)
+            .join(Project)
+            .join(Package, AccountProject.packages)
+        )  # Join with Package here
 
         # Apply search filters if provided
         query = cls.filter_by_search_criteria(query, search_options)
@@ -36,9 +42,7 @@ class ProjectQueries:
     @classmethod
     def get_projects_by_proponent_id(cls, proponent_id: int):
         """Find projects by proponent_id"""
-        query = db.session.query(Project).filter(
-            Project.proponent_id == proponent_id
-        )
+        query = db.session.query(Project).filter(Project.proponent_id == proponent_id)
         return query.all()
 
     @classmethod
@@ -47,9 +51,7 @@ class ProjectQueries:
         if not search_options:
             return query
 
-        query = cls._filter_by_submission_name(
-            query, search_options.search_text)
-
+        query = cls._filter_by_submission_name(query, search_options.search_text)
         query = cls._filter_by_submission_status(query, search_options.status)
 
         return query
@@ -58,9 +60,7 @@ class ProjectQueries:
     def _filter_by_submission_name(cls, query, search_text):
         """Filter by search text across package name."""
         if search_text:
-            query = query.join(Package, AccountProject.packages).filter(
-                Package.name.ilike(f"%{search_text}%")
-            )
+            query = query.filter(Package.name.ilike(f"%{search_text}%"))
         return query
 
     @classmethod
@@ -70,7 +70,5 @@ class ProjectQueries:
             # Convert enum to string values
             status_values = [status.value for status in statuses]
             if status_values:
-                query = query.join(Package, AccountProject.packages).filter(
-                    Package.status.op('&&')(status_values)
-                )
+                query = query.filter(Package.status.op("&&")(status_values))
         return query
